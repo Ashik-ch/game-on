82c04f160be19d0b9387dfb4ddde63952c465ae8
VAASITS <dev@vaasits.com>
2025-04-11 18:27:36 +0530

    feat: add HomeComponent with routing and initial tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { TurfFormComponent } from './admin/turf/turf-form/turf-form.component';
+import { HomeComponent } from './admin/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'turf/create', component: TurfFormComponent },
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
