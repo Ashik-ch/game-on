2127045c813fd67ac96359893349f700456ca9fe
VAASITS <dev@vaasits.com>
2025-04-15 12:18:06 +0530

    chore: update Angular dependencies to version 19.0.0 and adjust styles

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  standalone: true,
-  imports: [CommonModule,
-    RouterModule,],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    standalone: true,
+    imports: [CommonModule,
+        RouterModule,],
 })
 export class HomeComponent {
 
