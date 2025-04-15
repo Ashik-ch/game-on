88de8bf8965ac8c29de19dfc1d11919ce46a7d97
VAASITS <dev@vaasits.com>
2025-04-15 16:22:56 +0530

    feat: enhance TurfList component with improved table structure, search functionality, and loading state management

--- src/app/admin/turf/turf-list/turf-list.component.ts
+++ src/app/admin/turf/turf-list/turf-list.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { CommunicatorService } from '../../service';
 import { turfListApiJson } from '../api';
 import { TurfListType } from '../helper';
+import { CommonModule } from '@angular/common';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-turf-list',
   templateUrl: './turf-list.component.html',
   styleUrl: './turf-list.component.scss',
   standalone: true,
-  imports: [RouterModule,
-    TableModule
+  imports: [CommonModule,
+    RouterModule,
+    TableModule,
+    IconFieldModule,
+    InputIconModule,
+    SelectModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule
   ],
 })
 export class TurfListComponent {
+  isLoading = false;
   turfList: TurfListType[] = [];
   constructor(private communicatorService: CommunicatorService) { }
 
@@ -23,11 +38,18 @@ export class TurfListComponent {
   }
 
   getTurfList() {
+    this.isLoading = true;
     const turfListApiPayload = { ...turfListApiJson }
     this.communicatorService.apiRunner(turfListApiPayload).subscribe((apiReturn) => {
       if (apiReturn) {
         this.turfList = apiReturn
+        this.isLoading = false;
       }
     });
+    this.isLoading = false;
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
   }
 }
