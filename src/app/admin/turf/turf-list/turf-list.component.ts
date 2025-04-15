d4237975e812f0fa0fb9b333d712dca6279cab5e
VAASITS <dev@vaasits.com>
2025-04-15 12:02:27 +0530

    feat: update TurfList component with corrected column name and RouterModule import

--- src/app/admin/turf/turf-list/turf-list.component.ts
+++ src/app/admin/turf/turf-list/turf-list.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommunicatorService } from '../../../service/communicator/communicator.service';
 import { turfListApiJson } from '../api';
 import { TurfListType } from '../helper';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-turf-list',
-  standalone: true,
-  imports: [],
   templateUrl: './turf-list.component.html',
   styleUrl: './turf-list.component.scss',
+  standalone: true,
+  imports: [RouterModule],
 })
 export class TurfListComponent {
   turfList: TurfListType[] = [];
